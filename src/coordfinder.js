644d0409de2d13c3ce90e1fcfffa57f148a96423
aieuren <aieuren@users.noreply.github.com>
2026-01-15 19:35:23 +0000

    Update build number to 20260115-193515
    Co-authored-by: Ona <no-reply@ona.com>

--- src/coordfinder.js
+++ src/coordfinder.js
@@ -2095,7 +2095,7 @@ function CF(text, opts) {
 
 // Metadata
 CF.version = "5.0-beta.7";
-CF.build = "20260115-185512"; // Auto-updated by update-build.sh
+CF.build = "20260115-193515"; // Auto-updated by update-build.sh
 CF.author = "Bernt Rane, Claude & Ona";
 CF.license = "MIT";
 CF.ratingDefault = 0.5;
